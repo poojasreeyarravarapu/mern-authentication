efcb0c155f2690b1b4c584d6850dce123cdd8d40
POOJA SREE YARRAVARAPU <122040805+poojasreeyarravarapu@users.noreply.github.com>
2025-06-07 13:39:31 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <img src={assets.header_img} alt='' className='w-36 h-36 rounded-full mb-6'/>
       <h1 className='flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2'>Hey {userData ? userData.name : 'Developer'} <img className='w-8 aspect-square' src={assets.hand_wave} alt=''/></h1>
       <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>Welcome to our app</h2>
-      <p className='mb-8 max-w-md'>Lets start a quick proproduct tour and we will have you up and running in no time!</p>
+      <p className='mb-8 max-w-md'>Lets start a quick product tour and we will have you up and running in no time!</p>
       <button className='border border-gray-800 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all'>Get Started</button>
     </div>
   )
